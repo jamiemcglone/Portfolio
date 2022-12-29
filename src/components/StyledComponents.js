f2aca4cab4f6ca74bab1191dcc1ac61b7a296ac8
jamiemcglone <jamie.mcglone21@btinternet.com>
2022-12-29 16:01:57 +0000

    Added background colours

--- src/components/StyledComponents.js
+++ src/components/StyledComponents.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Paragraph = styled.p`
 text-align: center;
-color: whitesmoke;
+color: black;
 font-family: 'futura', sans-serif;
 font-size: 1.5rem;
 
